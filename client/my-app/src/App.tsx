80ea68e64351947d8a9d078feada9e69f79d15f6
NguyenVoHuyToan <huytoan9295@gmail.com>
2024-02-23 21:22:29 +0700

    new

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import Layout from "./layout/Layout";
+import SignIn from "./auth/SignIn";
+import SignUp from "./auth/SignUp";
+import Products from "./page/Products";
+import Detail from "./page/Detail";
+import Payment from "./page/Payment";
+import Cart from "./page/Cart";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-        <Route path="/" element={<></>}></Route>
-        </Routes>
-      </BrowserRouter>
-    
-    </div>
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/detail/:ID" element={<Detail />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/cart/:ID" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
