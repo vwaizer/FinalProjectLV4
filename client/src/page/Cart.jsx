bd4c40eb9ec0f532eb1d62c4c7c2f3480d4812eb
ybinh07 <yenbinh18082307@gmail.com>
2024-03-01 19:45:04 +0700

    cm

--- client/src/page/Cart.jsx
+++ client/src/page/Cart.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import Layout from '../layout/Layout'
-
+import React from "react";
+import Layout from "../layout/Layout";
+import "./cart.css";
 function Cart() {
+  const books = [];
   return (
-    <Layout>Cart</Layout>
-  )
+    <Layout>
+      <div className="cart-container">
+        <div className="header-cart-item">
+          <h2>GIỎ HÀNG ({books.length} sản phẩm)</h2>
+          <div className="checked-all-products">
+            <div style={{ flex: "2" }}>
+              <label>
+                <input type="checkbox" />
+                <span style={{ margin: "0px 5px" }}>
+                  Chọn tất cả ({books.length} sản phẩm)
+                </span>
+              </label>
+            </div>
+
+            <div style={{ flex: ".5" }}>Số lượng</div>
+            <div style={{ flex: ".5" }}>Thành tiền</div>
+          </div>
+
+          <div style={{border:'1px solid green'}}>
+            <label>
+              <input type="checkbox"/>
+            </label>
+          </div>
+        </div>
+
+        <div style={{ flex: "1", border: "1px solid violet" }}>
+          <h2>Tổng số tiền</h2>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
-export default Cart
+export default Cart;
--- client/src/page/Products.jsx
+++ client/src/page/Products.jsx
@@ -1,15 +1,68 @@
-import React from "react";
+
 import Layout from "../layout/Layout";
 import { Menu } from "antd";
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 
+import React, { useState } from 'react';
 function Products() {
+  
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem('Option 1', '1'),
+    getItem('Option 2', '2'),
+    getItem('Option 3', '3'),
+    getItem('Option 4', '4'),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
+  ]),
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+];
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+  const [openKeys, setOpenKeys] = useState(['sub1']);
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (latestOpenKey && rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
   return (
     <Layout>
       <div className="bookstore_wrap">
         <div className="leftbox">
-          <div className="leftbox_collection"></div>
+          <div className="leftbox_collection">
+          <Menu
+      mode="inline"
+      openKeys={openKeys}
+      onOpenChange={onOpenChange}
+      style={{
+        width: 256,
+      }}
+      items={items}
+    />
+          </div>
         </div>
       </div>
+
+
     </Layout>
   );
 }
