8648a96e830ad0d62e8560daf663941e3b680db8
vwaizer <lightwing2208@gmail.com>
2024-03-10 21:03:11 +0700

    fix detail

--- client/src/page/Detail.jsx
+++ client/src/page/Detail.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Layout from '../layout/Layout'
-import './page.css'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import Layout from "../layout/Layout";
+import "./page.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Detail(props) {
-  const {id} = useParams("ID")
-  const [product, setProduct] = useState([])
-  console.log(id)
+ 
+  const { ID } = useParams("ID"); 
+
+  const [product, setProduct] = useState();
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/book/detailBook/${id} `)
+      .get(`http://localhost:4000/book/detailBook/${ID}`)
       .then((product) => setProduct(product.data))
       .catch((err) => console.log(err));
-    }, []);
-    console.log(product)
+  }, []);
+  console.log(product);
   return (
     <Layout>
-     <div>
-      {}
-     </div>
+      <div></div>
     </Layout>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
