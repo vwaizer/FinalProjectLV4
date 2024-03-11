6b30680bb528a583afbe646cf35d2cfd9e0735a9
ybinh07 <yenbinh18082307@gmail.com>
2024-03-11 17:31:51 +0700

    cart

--- client/src/page/Cart.jsx
+++ client/src/page/Cart.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import Layout from "../layout/Layout";
 import "./cart.css";
+import { useState } from "react";
+
 function Cart() {
-  const books = [];
+  const books = [
+    {
+      bookImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShc1Hd9nzZbrP_1N07MrjGbekp8Pz6J4HRHtw-FGB33Q&s",
+      name: "book",
+      price: 100000,
+    },
+    {
+      bookImage:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShc1Hd9nzZbrP_1N07MrjGbekp8Pz6J4HRHtw-FGB33Q&s",
+      name: "books",
+      price: 100000,
+    },
+  ];
+
+  const [inputValue, setInputValue] = useState({});
+
+  const handleKeyPress = (event, name) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      const value = parseInt(event.target.value);
+      if (!isNaN(value)) {
+        const newInputValue = { ...inputValue };
+        newInputValue[name] = value;
+        setInputValue(newInputValue);
+      }
+    }
+  };
+
+  const handleInputChange = (event, name) => {
+    const value = parseInt(event.target.value);
+    if (!isNaN(value)) {
+      const newInputValue = { ...inputValue };
+      newInputValue[name] = value;
+      setInputValue(newInputValue);
+    }
+  };
   return (
     <Layout>
       <div className="cart-container">
@@ -17,20 +55,77 @@ function Cart() {
                 </span>
               </label>
             </div>
-
             <div style={{ flex: ".5" }}>Số lượng</div>
-            <div style={{ flex: ".5" }}>Thành tiền</div>
+            <div style={{ flex: ".5", textAlign: "center" }}>Thành tiền</div>
           </div>
 
-          <div style={{border:'1px solid green'}}>
-            <label>
-              <input type="checkbox"/>
-            </label>
+          <div >
+            {books.map((item) => {
+              const { bookImage, name, price } = item;
+              return (
+                <div>
+                <div className="product-container">
+                  <div className="amount-container">
+                    <input type="checkbox" />
+                  </div>
+                  <div >
+                    <img className="books-image" src={bookImage} alt=""></img>
+                  </div>
+                  <div className="products-info">
+                    <div>{name}</div>
+                    <div>{price}</div>
+                  </div>
+                  {/* nút tăng giảm số lượng */}
+                  <div style={{  flex: ".5" }}>
+                    <div className="amount-button">
+                     <div>
+                     
+                      <button
+                        onClick={() => {
+                          if (inputValue[name] > 1) {
+                            const newInputValue = { ...inputValue };
+                            newInputValue[name] =
+                              (newInputValue[name] || 0) - 1;
+                            setInputValue(newInputValue);
+                          }
+                        }}
+                      >
+                        <img  src="https://cdn0.fahasa.com/skin//frontend/ma_vanese/fahasa/images/ico_minus2x.png" alt=""></img>
+                      </button>
+                      <input
+                        type="text"
+                        value={inputValue[name] || ""}
+                        onInput={handleKeyPress}
+                        onChange={(event) => handleInputChange(event, name)}
+                      />
+                      <button
+                        onClick={() => {
+                          const newInputValue = { ...inputValue };
+                          newInputValue[name] = (newInputValue[name] || 0) + 1;
+                          setInputValue(newInputValue);
+                        }}
+                      >
+                        <img src="https://cdn0.fahasa.com/skin/frontend/ma_vanese/fahasa/images/ico_plus2x.png" alt="" />
+                      </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="product-price">
+                    {price * inputValue[name] || 0}
+                  </div>
+                
+                </div>
+                <hr style={{border:'1px solid #C0C0C0'}}></hr> 
+                </div>
+              );
+            })}
+            
           </div>
+         
         </div>
-
+            
         <div style={{ flex: "1", border: "1px solid violet" }}>
-          <h2>Tổng số tiền</h2>
+          <h2 >Tổng số tiền</h2>
         </div>
       </div>
     </Layout>
--- client/src/page/Products.jsx
+++ client/src/page/Products.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 
 import Layout from "../layout/Layout";
 import { Menu } from "antd";
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 
 import React, { useState } from 'react';
-=======
-import Layout from "../layout/Layout";
-import { Menu } from "antd";
-import "./page.css";
-
-import React, { useState } from "react";
->>>>>>> 483d4e0e24942b3ceb7084994b67297922ca4dde
 function Products() {
   
 function getItem(label, key, icon, children, type) {
@@ -57,7 +49,6 @@ const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
       <div className="bookstore_wrap">
         <div className="leftbox">
           <div className="leftbox_collection">
-<<<<<<< HEAD
           <Menu
       mode="inline"
       openKeys={openKeys}
@@ -67,37 +58,6 @@ const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
       }}
       items={items}
     />
-=======
-            <div className="product">
-              <h2>Product Category</h2>
-              {/* 'map tu database' */}
-            </div>
-            <div className="author">
-              <h2>Author</h2>
-              {/* 'map tu database' */}
-            </div>
-            <div>
-              <h2>Publisher</h2>
-              {/* 'map tu database' */}
-            </div>
-          </div>
-        </div>
-        <div className="rigtbox">
-          <div className="rightbox_collection">
-            <div className="sort">
-              <p className="sort-name">
-                Sort by:
-                <select className="selection">
-                  <option>Name</option>
-                  <option>date</option>
-                </select>
-              </p>
-            </div>
-            <div className="product_info">
-              <h1>hello, this space uses for product information</h1>
-            {/* 'map tu database hình ảnh, tên spham, gia tien' */}
-            </div>
->>>>>>> 483d4e0e24942b3ceb7084994b67297922ca4dde
           </div>
         </div>
       </div>
