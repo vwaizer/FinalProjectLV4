09b186164a19f5761be9180af32253faba168ca1
vwaizer <lightwing2208@gmail.com>
2024-03-12 22:07:55 +0700

    add to cart

--- server/routes/receiptRoute.js
+++ server/routes/receiptRoute.js
@@ -1,7 +1,8 @@
 import express  from "express";
 import { addToCart, getFilterReceipt } from "../mongodb/service/receiptService.js";
+import { userValidator } from "../middleware/validator/roleValidator.js";
 
 export const receiptRoute=express.Router();
 receiptRoute.get("/:userID",getFilterReceipt)
-receiptRoute.post("/addToCart/:ID",addToCart)
+receiptRoute.post("/addToCart/:ID",userValidator,addToCart)
 
